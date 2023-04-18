e9259e98b0c0fe88a8048b0b3c7a3dcf6735bc3a
karls27185 <karls27185@gmail.com>
2023-04-19 01:54:23 +0300

    00:28:12

--- src/main/java/com/javastart/hibernatetest/dao/AccountDAO.java
+++ src/main/java/com/javastart/hibernatetest/dao/AccountDAO.java
@@ -1,9 +1,35 @@
 package com.javastart.hibernatetest.dao;
 
+import com.javastart.hibernatetest.entity.Account;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AccountDAO {
 
-    private 
+    private final SessionFactory sessionFactory;
+
+    @Autowired
+    public AccountDAO(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
+    public Account save(Account account) {
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+        session.save(account);
+        session.flush();
+        session.getTransaction().commit();
+        session.close();
+        return account;
+    }
+
+    public Account findById(Long id) {
+        Session session = sessionFactory.openSession();
+        Account account = session.find(Account.class, id);
+        session.close();
+        return account;
+    }
 }
