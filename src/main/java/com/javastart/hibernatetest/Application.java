8f0d68e4d87021d644fb3b569c6f015b04977c12
karls27185 <karls27185@gmail.com>
2023-04-20 03:07:16 +0300

    00:37:47

--- src/main/java/com/javastart/hibernatetest/Application.java
+++ src/main/java/com/javastart/hibernatetest/Application.java
@@ -1,11 +1,29 @@
 package com.javastart.hibernatetest;
 
+import com.javastart.hibernatetest.dao.AccountDAO;
+import com.javastart.hibernatetest.entity.Account;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
-public class Application {
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class,
+        JpaRepositoriesAutoConfiguration.class})
+public class Application implements CommandLineRunner {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Autowired
+    private AccountDAO accountDAO;
+
+    @Override
+    public void run(String... args) throws Exception {
+        Account account = new Account("Lori", 15);
+        Account saveAccount = accountDAO.save(account);
+
+        System.out.println(accountDAO.findById(saveAccount.getAccountId()));
     }
 }
